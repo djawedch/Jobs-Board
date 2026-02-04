87d5f6969191e732539a3f2c54c2c01ee82015d3
djawedch <120687949+djawedch@users.noreply.github.com>
2026-02-04 23:30:03 +0100

    Small Modifications

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -5,15 +5,17 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Http\Requests\RegisterUserRequest;
 use Illuminate\Support\Facades\{Auth, DB, Hash};
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class RegisteredUserController extends Controller
 {
-    public function create()
+    public function create(): View
     {
         return view('auth.register');
     }
 
-    public function store(RegisterUserRequest $request)
+    public function store(RegisterUserRequest $request): RedirectResponse
     {
         $user = DB::transaction(function () use ($request) 
         {
@@ -22,7 +24,7 @@ class RegisteredUserController extends Controller
             $user = User::create([
                 'name' => $validated['name'],
                 'email' => $validated['email'],
-                'password' => Hash::make($validated['password']),
+                'password' => $validated['password'],
             ]);
 
             $logoPath = $request->file('logo')->store('logos');
